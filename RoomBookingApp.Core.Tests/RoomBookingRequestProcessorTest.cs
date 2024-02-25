a925e4780fc5b54f8fd59e1108ee12d7a1b0f775
Yazid Abughoush <yazid@ha2a.com>
2024-02-25 05:23:25 +0300

    Add project files.

---
+++ RoomBookingApp.Core.Tests/RoomBookingRequestProcessorTest.cs
@@ -0,0 +1,38 @@
+﻿using RoomBookingApp.Core.Models;
+using RoomBookingApp.Core.Processors;
+using Shouldly;
+
+namespace RoomBookingApp.Core.Tests
+{
+
+    public class RoomBookingRequestProcessorTest
+    {
+        [Fact]
+        public void Should_Return_Room_Booking_Response_With_Reques_Values()
+        {
+            // Arrange
+            var _request = new RoomBookingRequest
+            {
+                FullName = "Test Name",
+                Email = "tast@email.com",
+                Date = DateTime.Now
+            };
+
+            var _processor = new RoomBookingRequestProcessor();
+
+            // Act
+            RoomBookingResponse _result = _processor.BookRoom(_request);
+
+            // Assert
+            Assert.NotNull(_result);
+            Assert.Equal(_request.FullName, _result.FullName);
+            Assert.Equal(_request.Email, _result.Email);
+            Assert.Equal(_request.Date, _result.Date);
+
+            _result.ShouldNotBeNull("Result should not be null");
+            _result.FullName.ShouldBe(_result.FullName);
+            _result.Email.ShouldBe(_result.Email);
+            _result.Date.ShouldBe(_result.Date);
+        }
+    }
+}
---
+++ RoomBookingApp.Core/Processors/RoomBookingRequestProcessor.cs
@@ -0,0 +1,21 @@
+﻿using RoomBookingApp.Core.Models;
+
+namespace RoomBookingApp.Core.Processors
+{
+    public class RoomBookingRequestProcessor
+    {
+        public RoomBookingRequestProcessor()
+        {
+        }
+
+        public RoomBookingResponse BookRoom(RoomBookingRequest request)
+        {
+            return new RoomBookingResponse
+            {
+                FullName = request.FullName,
+                Email = request.Email,
+                Date = request.Date,
+            };
+        }
+    }
+}

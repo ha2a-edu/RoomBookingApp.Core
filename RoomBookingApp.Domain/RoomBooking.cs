dbea3702e146067d9e2db1300b5285277f29e7f5
Yazid Abughoush <yazid@ha2a.com>
2024-02-27 07:36:54 +0300

    refactoring for testing data access layer

--- RoomBookingApp.Domain/RoomBooking.cs
+++ RoomBookingApp.Domain/RoomBooking.cs
@@ -5,6 +5,8 @@ namespace RoomBookingApp.Domain
     public class RoomBooking : RoomBookingBase
     {
         public int Id { get; set; }
+
+        public Room Room { get; set; }
         public int RoomId { get; set; }
     }
 }
---
+++ RoomBookingApp.Persistance/Repositories/RoomBookingService.cs
@@ -0,0 +1,23 @@
+﻿using RoomBookingApp.DataServices;
+using RoomBookingApp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomBookingApp.Persistance.Repositories
+{
+    public class RoomBookingService : IRoomBookingService
+    {
+        public IEnumerable<Room> GetAvailableRooms(DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(RoomBooking roomBooking)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

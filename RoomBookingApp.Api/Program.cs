dfafa850e1d5f733785a1b8ff712a0b09982be2f
Yazid Abughoush <yazid@ha2a.com>
2024-03-01 06:48:43 +0300

    testing gitlab branch protection

--- RoomBookingApp.Api/Program.cs
+++ RoomBookingApp.Api/Program.cs
@@ -8,7 +8,6 @@ using RoomBookingApp.Persistance.Repositories;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
